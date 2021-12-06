12ceff56f9e0b4ad667eac3c1d20a71ac4b93364
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-12-06 12:04:19 +0100

    completed with function set and get

--- classes/Products.php
+++ classes/Products.php
@@ -1,5 +1,58 @@
 <?php
+
+
     class Products {
         
+        public $img = "";
+        public $name = "";
+        public $price = "";
+        public $description = "";
+
+        public function setImgProduct($productImg){
+           $this -> img =$productImg;
+        }
+
+        public function setNameProduct($productName){
+            var_dump($productName);
+            checkString($productName);
+            $this -> name = $productName;
+        }
+
+        public function setPriceProduct($productPrice) {
+            if(!ctype_digit($productPrice)){
+                exit("Prezzo prodotto non valido");
+            }
+
+            $this -> price = $productPrice . ' €';
+        }
+        
+        public function setDescriptionProduct($productDescription) {
+            
+            checkString($productDescription);
+            
+            if(strlen($productDescription) <= 15){
+                exit("Descrizione prodotto troppo corta");
+            }
+
+            $this -> description = $productDescription;
+        }
+
+        public function getImgProduct(){
+            return "./images/" .  $this -> img;
+        }
+
+        public function getNameProduct(){
+            return $this->name;
+        }
+
+        public function getPriceProduct(){
+            return $this -> price;
+        }
+
+        public function getDescriptionProduct(){
+            return $this -> description;
+        }
     }
+
+    
 ?>
